85b71e3d580605a002f967668d0f0e4d15b9a30c
mateogic <ljc_2318516761@163.com>
2025-05-19 15:03:30 +0800

    搭建后端工程：API请求日志切面、参数校验、全局异常管理、自定义 Jackson 序列化

---
+++ blog-module-common/src/main/java/cn/mateogic/blog/common/exception/BaseExceptionInterface.java
@@ -0,0 +1,6 @@
+package cn.mateogic.blog.common.exception;
+// 基础异常接口
+public interface BaseExceptionInterface {
+    String getErrorCode();
+    String getErrorMessage();
+}
--- blog-web/src/main/java/cn/mateogic/blog/web/controller/TestController.java
+++ blog-web/src/main/java/cn/mateogic/blog/web/controller/TestController.java
@@ -1,19 +1,44 @@
 package cn.mateogic.blog.web.controller;
 
+import cn.mateogic.blog.common.enums.ResponseCodeEnum;
+import cn.mateogic.blog.common.exception.BizException;
+import cn.mateogic.blog.common.utils.JsonUtil;
+import cn.mateogic.blog.common.utils.Response;
 import cn.mateogic.blog.web.model.User;
 import cn.mateogic.blog.common.aspect.ApiOperationLog;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.stream.Collectors;
+
 // 测试日志注解
 @RestController
 @Slf4j
+@Api(tags = "首页模块")
 public class TestController {
     @PostMapping("/test")
-    @ApiOperationLog(description = "测试日志注解")
-    public User test(@RequestBody User user) {
-        return user;
+    @ApiOperationLog(description = "测试接口")
+    @ApiOperation(value = "测试接口")
+    public Response test(@RequestBody @Validated User user) {
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
+
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
+
+        return Response.success(user);
     }
 }

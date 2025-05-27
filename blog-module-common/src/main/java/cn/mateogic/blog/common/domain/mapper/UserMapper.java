0b86112f0be0a799d01835f2ba179716ad6e2839
mateogic <ljc_2318516761@163.com>
2025-05-27 15:15:09 +0800

    实现 JWT 身份验证模块，包括登录认证、Token 生成与解析

--- blog-module-common/src/main/java/cn/mateogic/blog/common/domain/mapper/UserMapper.java
+++ blog-module-common/src/main/java/cn/mateogic/blog/common/domain/mapper/UserMapper.java
@@ -1,6 +1,12 @@
 package cn.mateogic.blog.common.domain.mapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import cn.mateogic.blog.common.domain.dos.UserDO;
 
 public interface UserMapper extends BaseMapper<UserDO> {
+    default UserDO findByUsername(String username) {
+        LambdaQueryWrapper<UserDO> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(UserDO::getUsername, username);
+        return selectOne(wrapper);
+    }
 }

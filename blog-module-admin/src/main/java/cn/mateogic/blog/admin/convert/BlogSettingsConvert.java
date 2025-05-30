c83d027cfdd4213c7410b81406c265851e0adc81
mateogic <ljc_2318516761@163.com>
2025-05-30 18:11:38 +0800

    简化属性映射

---
+++ blog-module-admin/src/main/java/cn/mateogic/blog/admin/convert/BlogSettingsConvert.java
@@ -0,0 +1,22 @@
+package cn.mateogic.blog.admin.convert;
+
+import cn.mateogic.blog.admin.model.vo.blogsettings.UpdateBlogSettingsReqVO;
+import cn.mateogic.blog.common.domain.dos.BlogSettingsDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BlogSettingsConvert {
+    /**
+     * 初始化 convert 实例
+     */
+    BlogSettingsConvert INSTANCE = Mappers.getMapper(BlogSettingsConvert.class);
+
+    /**
+     * 将 VO 转化为 DO
+     * @param bean
+     * @return
+     */
+    BlogSettingsDO convertVO2DO(UpdateBlogSettingsReqVO bean);
+
+}
--- blog-module-admin/src/main/java/cn/mateogic/blog/admin/service/impl/AdminBlogSettingsServiceImpl.java
+++ blog-module-admin/src/main/java/cn/mateogic/blog/admin/service/impl/AdminBlogSettingsServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.mateogic.blog.admin.service.impl;
 
+import cn.mateogic.blog.admin.convert.BlogSettingsConvert;
 import cn.mateogic.blog.admin.model.vo.blogsettings.UpdateBlogSettingsReqVO;
 import cn.mateogic.blog.admin.service.AdminBlogSettingsService;
 import cn.mateogic.blog.common.domain.dos.BlogSettingsDO;
@@ -14,18 +15,8 @@ public class AdminBlogSettingsServiceImpl extends ServiceImpl<BlogSettingsMapper
     @Override
     public Response updateBlogSettings(UpdateBlogSettingsReqVO updateBlogSettingsReqVO) {
         // VO 转 DO
-        BlogSettingsDO blogSettingsDO = BlogSettingsDO.builder()
-                .id(1L)
-                .logo(updateBlogSettingsReqVO.getLogo())
-                .name(updateBlogSettingsReqVO.getName())
-                .author(updateBlogSettingsReqVO.getAuthor())
-                .introduction(updateBlogSettingsReqVO.getIntroduction())
-                .avatar(updateBlogSettingsReqVO.getAvatar())
-                .githubHomepage(updateBlogSettingsReqVO.getGithubHomepage())
-                .giteeHomepage(updateBlogSettingsReqVO.getGiteeHomepage())
-                .csdnHomepage(updateBlogSettingsReqVO.getCsdnHomepage())
-                .zhihuHomepage(updateBlogSettingsReqVO.getZhihuHomepage())
-                .build();
+        BlogSettingsDO blogSettingsDO = BlogSettingsConvert.INSTANCE.convertVO2DO(updateBlogSettingsReqVO);
+        blogSettingsDO.setId(1L);
 
         // 保存或更新（当数据库中存在 ID 为 1 的记录时，则执行更新操作，否则执行插入操作）
         saveOrUpdate(blogSettingsDO);

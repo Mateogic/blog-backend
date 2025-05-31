8506b9e750f8ebb827acaaf6fc932e4386e31655
mateogic <ljc_2318516761@163.com>
2025-05-31 16:22:50 +0800

    获取文章详情接口开发

--- blog-web/src/main/java/cn/mateogic/blog/web/service/ArticleService.java
+++ blog-web/src/main/java/cn/mateogic/blog/web/service/ArticleService.java
@@ -1,6 +1,7 @@
 package cn.mateogic.blog.web.service;
 
 import cn.mateogic.blog.common.utils.Response;
+import cn.mateogic.blog.web.model.vo.article.FindArticleDetailReqVO;
 import cn.mateogic.blog.web.model.vo.article.FindIndexArticlePageListReqVO;
 
 public interface ArticleService {
@@ -10,4 +11,10 @@ public interface ArticleService {
      * @return
      */
     Response findArticlePageList(FindIndexArticlePageListReqVO findIndexArticlePageListReqVO);
+    /**
+     * 获取文章详情
+     * @param findArticleDetailReqVO
+     * @return
+     */
+    Response findArticleDetail(FindArticleDetailReqVO findArticleDetailReqVO);
 }

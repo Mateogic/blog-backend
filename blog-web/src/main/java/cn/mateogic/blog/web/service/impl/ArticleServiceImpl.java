f4150d3db130a0eb41f7b93518aa8ad97920b3b4
mateogic <ljc_2318516761@163.com>
2025-06-02 22:31:41 +0800

    正则表达式统计 Markdown 正文字数、阅读时长

--- blog-web/src/main/java/cn/mateogic/blog/web/service/impl/ArticleServiceImpl.java
+++ blog-web/src/main/java/cn/mateogic/blog/web/service/impl/ArticleServiceImpl.java
@@ -13,6 +13,7 @@ import cn.mateogic.blog.web.model.vo.article.*;
 import cn.mateogic.blog.web.model.vo.category.FindCategoryListRspVO;
 import cn.mateogic.blog.web.model.vo.tag.FindTagListRspVO;
 import cn.mateogic.blog.web.service.ArticleService;
+import cn.mateogic.blog.web.utils.MarkdownStatsUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.google.common.collect.Lists;
@@ -157,13 +158,17 @@ public class ArticleServiceImpl implements ArticleService {
 
         // 查询正文
         ArticleContentDO articleContentDO = articleContentMapper.selectByArticleId(articleId);
-
+        String content = articleContentDO.getContent();
+        // 计算 md 正文字数
+        Integer totalWords = MarkdownStatsUtil.calculateWordCount(content);
         // DO 转 VO
         FindArticleDetailRspVO vo = FindArticleDetailRspVO.builder()
                 .title(articleDO.getTitle())
                 .createTime(articleDO.getCreateTime())
-                .content(MarkdownHelper.convertMarkdown2Html(articleContentDO.getContent()))
+                .content(MarkdownHelper.convertMarkdown2Html(content))
                 .readNum(articleDO.getReadNum())
+                .totalWords(totalWords)
+                .readTime(MarkdownStatsUtil.calculateReadingTime(totalWords))
                 .build();
 
         // 查询所属分类

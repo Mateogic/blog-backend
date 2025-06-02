6dcb66ca2b544556e451e2f7fb43c6d58340c6ef
mateogic <ljc_2318516761@163.com>
2025-06-02 22:19:06 +0800

    启用定时任务

--- blog-web/src/main/java/cn/mateogic/blog/web/BlogWebApplication.java
+++ blog-web/src/main/java/cn/mateogic/blog/web/BlogWebApplication.java
@@ -3,9 +3,11 @@ package cn.mateogic.blog.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan({"cn.mateogic.blog.*"}) // 多模块项目中，必需手动指定扫描
+@EnableScheduling // 启用定时任务
 public class BlogWebApplication {
 
     public static void main(String[] args) {

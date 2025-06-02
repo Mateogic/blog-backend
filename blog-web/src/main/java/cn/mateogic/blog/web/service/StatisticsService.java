5468891d5f95b2e3f19028a0d721bf0c7f6b4d02
mateogic <ljc_2318516761@163.com>
2025-06-02 18:06:32 +0800

    更新统计信息功能，添加统计信息响应类及相关服务实现

---
+++ blog-web/src/main/java/cn/mateogic/blog/web/service/StatisticsService.java
@@ -0,0 +1,11 @@
+package cn.mateogic.blog.web.service;
+
+import cn.mateogic.blog.common.utils.Response;
+
+public interface StatisticsService {
+    /**
+     * 获取文章总数、分类总数、标签总数、总访问量统计信息
+     * @return
+     */
+    Response findInfo();
+}
